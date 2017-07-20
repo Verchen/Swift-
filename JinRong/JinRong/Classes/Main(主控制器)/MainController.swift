200d1e3a59d91abebe3d7b78fffef2834c6616af
qiao <iosqiao@qq.com>
2017-07-20 18:06:13 +0800

    更换tabbar图片

--- JinRong/JinRong/Classes/Main(主控制器)/MainController.swift
+++ JinRong/JinRong/Classes/Main(主控制器)/MainController.swift
@@ -20,33 +20,33 @@ class MainController: UITabBarController {
     func setupChildVC() -> Void {
         let borrowVC = BorrowController()
         borrowVC.title = "借款"
-        borrowVC.tabBarItem.image = UIImage(named: "tabbar_home")
-        borrowVC.tabBarItem.selectedImage = UIImage(named: "tabbar_home_highlighted")?.withRenderingMode(.alwaysOriginal)
-        borrowVC.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.orange], for: .selected)
+        borrowVC.tabBarItem.image = UIImage(named: "tabbar_home_normal")
+        borrowVC.tabBarItem.selectedImage = UIImage(named: "tabbar_home_select")?.withRenderingMode(.alwaysOriginal)
+        borrowVC.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.theme], for: .selected)
         let nav1 = BaseNavigationController(rootViewController: borrowVC)
         self.addChildViewController(nav1)
         
         let authVC = AuthController()
         authVC.title = "认证"
-        authVC.tabBarItem.image = UIImage(named: "tabbar_discover")
-        authVC.tabBarItem.selectedImage = UIImage(named: "tabbar_discover_highlighted")?.withRenderingMode(.alwaysOriginal)
-        authVC.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.orange], for: .selected)
+        authVC.tabBarItem.image = UIImage(named: "tabbar_auth_normal")
+        authVC.tabBarItem.selectedImage = UIImage(named: "tabbar_auth_select")?.withRenderingMode(.alwaysOriginal)
+        authVC.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.theme], for: .selected)
         let nav2 = BaseNavigationController(rootViewController: authVC)
         self.addChildViewController(nav2)
         
         let billVC = BillController()
         billVC.title = "账单"
-        billVC.tabBarItem.image = UIImage(named: "tabbar_message_center")
-        billVC.tabBarItem.selectedImage = UIImage(named: "tabbar_message_center_highlighted")?.withRenderingMode(.alwaysOriginal)
-        billVC.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.orange], for: .selected)
+        billVC.tabBarItem.image = UIImage(named: "tabbar_zhangdan_normal")
+        billVC.tabBarItem.selectedImage = UIImage(named: "tabbar_zhangdan_select")?.withRenderingMode(.alwaysOriginal)
+        billVC.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.theme], for: .selected)
         let nav3 = BaseNavigationController(rootViewController: billVC)
         self.addChildViewController(nav3)
         
         let profileVC = ProfileController()
         profileVC.title = "我的"
-        profileVC.tabBarItem.image = UIImage(named: "tabbar_profile")
-        profileVC.tabBarItem.selectedImage = UIImage(named: "tabbar_profile_highlighted")?.withRenderingMode(.alwaysOriginal)
-        profileVC.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.orange], for: .selected)
+        profileVC.tabBarItem.image = UIImage(named: "tabbar_wo_normal")
+        profileVC.tabBarItem.selectedImage = UIImage(named: "tabbar_wo_select")?.withRenderingMode(.alwaysOriginal)
+        profileVC.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.theme], for: .selected)
         let nav4 = BaseNavigationController(rootViewController: profileVC)
         self.addChildViewController(nav4)
         
